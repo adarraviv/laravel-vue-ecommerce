afff4f7fe27c49dfc5688802668ca597d4aab668
adarraviv <adarraviv@gmail.com>
2022-07-18 12:11:40 -0700

    Initial commit

---
+++ resources/js/components/AddToCart.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <hr>
+        <button class="btn btn-warning" v-on:click.prevent="addProductToCart()">Add to Cart</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){},
+        props:['productId','userId'],
+        methods:{
+            addProductToCart(){
+                alert(this.productId);
+            }
+        },
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
